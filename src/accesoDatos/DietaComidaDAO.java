b93d72895f818083c6ab2cf65942f3ec543fdcd1
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2023-10-14 12:48:11 -0300

    DietaComida
    Metodo dao para buscar

--- src/accesoDatos/DietaComidaDAO.java
+++ src/accesoDatos/DietaComidaDAO.java
@@ -5,10 +5,39 @@
  */
 package accesoDatos;
 
+import entidades.DietaComida;
+import entidades.Horario;
+import javax.swing.JOptionPane;
+import service.ComidaService;
+import service.DietaService;
+
 /**
  *
  * @author criss
  */
-public class DietaComidaDAO {
-    
+public final class DietaComidaDAO extends DAO {
+
+    public DietaComida buscarDietaComidaBaseDatos(int idDietaComida) {
+        try {
+            String sql = "SELECT `idDietaComida`, `idComida`, `idDieta`, `Horario` FROM `dietacomida` WHERE idDietaComida = " + idDietaComida;
+            consultarBaseDatos(sql);
+            DietaComida aux = null;
+            ComidaService cs = new ComidaService();
+            DietaService ds = new DietaService();
+            String horarioStr = null;
+            while (resultado.next()) {
+                Integer IDComida = resultado.getInt(2);
+                Integer IDDieta = resultado.getInt(3);
+                horarioStr = resultado.getString(4);
+                Horario horarioEnum = Horario.valueOf(horarioStr);
+                aux = new DietaComida(idDietaComida, cs.buscarComida(IDComida), ds.buscarDietaPorId(IDDieta), horarioEnum);
+            }
+            return aux;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al buscar la dieta comida de la base de datos");
+        } finally {
+            desconectarBaseDatos();
+        }
+        return null;
+    }
 }
--- src/nutricionista/Nutricionista.java
+++ src/nutricionista/Nutricionista.java
@@ -6,6 +6,7 @@
 package nutricionista;
 
 import Vista.Principal;
+import accesoDatos.DietaComidaDAO;
 import java.time.LocalDate;
 import service.ComidaService;
 import service.DietaService;
@@ -39,6 +40,8 @@ public class Nutricionista {
         Principal p =new Principal();
         p.setVisible(true);
         p.setLocationRelativeTo(null);
+        DietaComidaDAO dao = new DietaComidaDAO();
+        System.out.println(dao.buscarDietaComidaBaseDatos(1));
     }
 
 }
